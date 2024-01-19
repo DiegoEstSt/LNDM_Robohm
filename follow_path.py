57cb21d2af0fddffb008b6b9ed8afb15c51f078a
RoboPi <diego.st@gmx.de>
2024-01-19 14:44:58 +0100

    Zwischen Speicherung, Code überprüft momentan einen Punkt horizontal und zwei vertikal am Bildschrimrand, noch nicht fähig der Linie zu folgen

--- follow_path.py
+++ follow_path.py
@@ -1,10 +1,13 @@
 from robot import Robot
 import numpy as np
 from time import sleep
+import time
 import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
 import picamera2
 import matplotlib.pyplot as plt
+from matplotlib.patches import Circle
 import sys
+import cv2
 
 # Wandelt einen Farbwert in einen  State (1: Schwarz, 2: Grün, 0: andere Farbe) um 
 def color_to_state(color):
@@ -20,6 +23,7 @@ def color_to_state(color):
         return 2
     else:
         return 0
+
 
 # Berechnet die durchschnittliche Farbe im Quadrat mit Seitenlänge region_size um die angegebene Position herum 
 def average_color_around_position(pixel_array, x, y, region_size):
@@ -39,86 +43,213 @@ def average_color_around_position(pixel_array, x, y, region_size):
 
 
 # Berechnet den State (1: Schwarz, 2: Grün, 0: andere Farbe) der durchschnittlichen Farbe des Pixels an der angegebenen Position und der Farbe aller Pixel py darüber und my darunter
-def average_vertical_state_around_position(pixel_array, x, y, py, my):
+def average_horizontal_state_around_position(pixel_array, x, y, py, my):
     x = max(0, x)
     x = min(pixel_array.shape[1], x)
     y_start = max(0, y - my)
     y_end = min(pixel_array.shape[0], y + py)
-    
-    region = pixel_array[y_start:y_end, x]
-    
-    average_color = np.mean(region, axis=0)
-        
-    return color_to_state(average_color)
-    
 
-    
+    region = pixel_array[y_start:y_end, x]
+
+    average_color = np.mean(region, axis=0)
+
+    return color_to_state(average_color)
+
+
+# Berechnet den State (1: Schwarz, 2: Grün, 0: andere Farbe) der durchschnittlichen Farbe des Pixels an der angegebenen Position und der Farbe aller Pixel px links und mx rechts
+def average_vertical_state_around_position(pixel_array, x, y, px, mx):
+    y = max(0, y)
+    y = min(pixel_array.shape[1], y)
+    x_start = max(0, x - mx)
+    x_end = min(pixel_array.shape[0], x + px)
+
+    region = pixel_array[y, x_start:x_end]
+
+    average_color = np.mean(region, axis=0)
+
+    return color_to_state(average_color)
+
+
+
+# Gibt die Position der Mitte der ersten linken schwarzen Linie in einem Array wieder. die Funktion funktioniert nicht mit einer 100% Genauigkeit, da sie ueber die Pixel in einem bestimmten Abstand iteriert um Zeit zu sparen. Wenn nichts gefunden wird, gibt die Funktion None zurueck
+def get_line_position(line):
+    # Iteriert als erstes in 10 Schritten ueber das Arrat, die Linie spannt sich ja ueber mindestens 10 pixel und es geht so schneller
+    for i in range(0, len(line), 10):
+        if line[i] == 1:
+            for j in range(i, len(line)):
+                if line[j] != 1:
+                    return round((i + j) / 2)
+            return round((i + len(line) - 1) / 2)
+
+    # Falls beim ersten Durchlauf nichts rauskam, ueberpruefen wir das Array nochmals mit einer hoeheren Genauigkeit
+    for i in range(3, len(line), 5):
+        if line[i] == 1:
+            for j in range(i, len(line)):
+                if line[j] != 1:
+                    return round((i + j) / 2)
+            return round((i + len(line) - 1) / 2)
+
+    print("Keine Linie gesehen")
+
+    return None
+
+def stop():
+    print("stop")
+    robot.stop_motors()
+    robot.light_for_seconds(1)
+    sys.exit()
+
+
+
 if __name__ == "__main__":
+
     robot = Robot()
 
     # Konfiguriert die Kamera des Robotes
-    config = robot.camera.create_still_configuration(main={"size": (400, 200)}, display="main")
+    config = robot.camera.create_still_configuration(main={"size": (700, 700)}, display="main")
     robot.camera.configure(config)
     robot.camera.start()
-    #sleep(100)
 
     GPIO.setmode(GPIO.BCM)
 
-    GPIO.setup(21, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.setup(20, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    robot.light_for_seconds(3)
 
-    while GPIO.input(21) != GPIO.HIGH:
+    while GPIO.input(20) != GPIO.HIGH:
         sleep(1)
-    print("Starting")
-    robot.beep_for_seconds(3)
     
+    print("Starting")
+    robot.light_for_seconds(3)
+    sleep(3)
+    #robot.turn(100)
+    #sleep(100)
+    
+    
+    frames = 0
+    start_time = time.time()
+    
+    # Speichert die Laenge des states array
+    state_size = 300
+
+    # Speichert die letze Position der Linie. kann entweder "left" oder "right" sein
+    last_line_position = ""
+    
+    # Stelle im Bildarray an der das horizontale States Array entnommen wird, Abstand vom oberen Bildrand
+    y_check_distance = 200
+    
+    # Distanz vom seitlichem Rand des Bildes, in dem die vertikalen States entnommen werden sollen
+    x_check_distance = 30
+    
+    # Bereich, der im unten im Bild entfernt wird. Bezieht sich auf den Abstand der Pixel im Originalbild (also nicht geresized). M
+    # Momentan nötig, da unten schwarzes Lego und der Schatten des Roboters zu sehen ist und wir auch vertikal im Bild überprüfen,
+    # sollten wir dies nicht mehr tun, kann darauf verzichtet werden den unteren Bereich zu entfernen
+    bottom_removal_distance = 30
+    
+
+
     while True:
         try:
-            pixel_array = robot.camera.capture_array("main")
+            pixel_array = cv2.resize(robot.camera.capture_array("main"), dsize= (state_size, state_size))
+            pixel_array = pixel_array[0:state_size - 1 - bottom_removal_distance]
+            # Ein Array, das ein horizontalen Ausschnitt aus dem Bild beinhaltet, der genutzt wird um den Roboter auf Kurs zu halten
             states = []
             for x in range(0, pixel_array.shape[1] - 1):
-                states.append(average_vertical_state_around_position(pixel_array, x, 180, 5, 5))
+                states.append(average_horizontal_state_around_position(pixel_array, x, y_check_distance, 5, 5))
+                
+            vl_states = []
+            vr_states = []
+            for y in range(0, pixel_array.shape[0] - 1):
+                vl_states.append(average_vertical_state_around_position(pixel_array, x_check_distance, y, 5, 5))
+                vr_states.append(average_vertical_state_around_position(pixel_array, state_size - x_check_distance - 1, y, 5, 5))
+                
+            line_position = get_line_position(states)
+            
+            vl_line_position = get_line_position(vl_states)
+            vr_line_position = get_line_position(vr_states)
             
             # Diese drei Zeilen dekommentieren, um eine grafische Übersicht über das State array zu bekommen
             #fig, ax = plt.subplots()
             #ax.stairs(states, linewidth=2.5)
             #plt.show()
+
+            # Diese Zeilen dekommentieren, um das aufgenommene Bild zusehen
+            fig, ax = plt.subplots(1)
+            ax.imshow(pixel_array)
+        
+            if line_position:
+                ax.add_patch(Circle((line_position, y_check_distance), radius=1, color="green"))
+            if vl_line_position:
+                ax.add_patch(Circle((x_check_distance, vl_line_position), radius=1, color="red"))
+            if vr_line_position:
+                ax.add_patch(Circle((state_size - 1 - x_check_distance, vr_line_position), radius=1, color="red"))
             
-            # Diese zwei Zeilen dekommentieren, um das aufgenommene Bild zusehen
-            #imgplt = plt.imshow(pixel_array)
-            #plt.show()
-            
+            plt.show()
+            continue
+
             # ----- Startet die Pfad Folgen Logic -------
-            
+
             # Dreht den Roboter
-            if any(states[0:50]):
-                robot.turn(-100)
-                robot.beep_for_seconds(0.5)
-            elif any(states[50:150]):
-                robot.turn(-100)
-            elif any(states[350:400]):
-                robot.turn(100)
-                robot.beep_for_seconds(0.5)
-            elif any(states[250:350]):
-                robot.turn(100)
+            robot.set_speed(65)
+            """
+            if any(states[0:30]):
+                robot.steer(-25)
+                #robot.light_for_seconds(0.5)
+                last_line_position = "left"
+            elif any(states[30:120]):
+                print("left")
+                robot.steer(-25)
+                last_line_position = "left"
+            elif any(states[270:300]):
+                robot.steer(25)
+                robot.light_for_seconds(0.5)
+                last_line_position = "right"
+            elif any(states[140:270]):
+                print("right")
+                robot.steer(25)
+                last_line_position = "right"
             elif any(states[150:250]):
-                robot.set_speed(100)
-                
-            # Wenn der Roboer nichts sieht, fährt er rückwärts
-            if not any(states):
-                robot.set_speed(-100)
+                robot.set_speed(50)
+                print("gerade")
+            else:
+                robot.set_speed(-30 if last_line_position == "left" else (30 if last_line_position == "right" else 0))"""
             
+            line_position = get_line_position(states)
+            print("Line position: ", line_position)
+            
+            if line_position != None:
+                if False:
+                    if line_position < ((state_size / 2) - 30):
+                        robot.steer(-100)
+                    elif line_position > ((state_size / 2) + 30):
+                        robot.steer(100)
+                    else:
+                        robot.steer(0)
+                else:
+                    if abs(line_position - (state_size / 2)) > 100:
+                        steering = int(max(-100, min( 100, (0.5 - (line_position / state_size)) * (-0.2))))
+                        print("Lenken mit dem Wert: " + str(steering))
+                        robot.steer(steering)
+                    else:
+                        robot.steer(0)
+
+            if 2 in states[0:130]:
+                print("left")
+               # robot.turn(-100)
+                #sleep(5)
+
+            if 2 in states[170:300]:
+                print("right")
+               # robot.turn(100)
+                #sleep(5)
+
+
             # Wenn der Knopf gedrückt wird, dann wird das Programm beendet
-            if GPIO.input(21) == GPIO.HIGH:
-                print("stop")
-                robot.motor_left.stop()
-                robot.motor_right.stop()
-                robot.beep_for_seconds(1)
-                sys.exit()
-                
+            if GPIO.input(20) == GPIO.HIGH:
+                stop()
+            
+            frames += 1
+            #print(frames / (time.time() - start_time))
+    
 
         except KeyboardInterrupt:
-            robot.motor_left.stop()
-            robot.motor_right.stop()
-            print("interrupt")
-            sys.exit()
-                   
+            stop()
--- robot.py
+++ robot.py
@@ -1,45 +1,126 @@
-from buildhat import Motor
+from buildhat import MotorPair, Motor
 from picamera2 import Picamera2
 import RPi.GPIO as GPIO
 from time import sleep
+import threading
 
 class Robot:
     def __init__(self):
-        self.motor_left = Motor("B")
-        self.motor_right = Motor("A")
+        # MotorPair hat bei unseren Testst dazu geführt, dass der eine Motor viel langsamer war
+        self.motor_left_1 = Motor("A")
+        self.motor_left_2 = Motor("B")
+        self.motor_right_1 = Motor("C")
+        self.motor_right_2 = Motor("D")
+
+        #self.steering = Motor("C")
         self.camera = Picamera2()
+        self.speed = 0
         # Startet den Buzzer
-        self.BUZZER_PIN = 6
+        self.LED_PIN = 21
         GPIO.setmode(GPIO.BCM)
-        GPIO.setup(self.BUZZER_PIN, GPIO.OUT, initial=GPIO.LOW)
+        GPIO.setup(self.LED_PIN, GPIO.OUT, initial=GPIO.LOW)
 
     #  Setzt die Geschwindigkeit vom linken Motor (Werte von -100 bis 100)
     #  Diese Funktion existiert, da die Geschwindigkeit aufgrund der räumlichen Positionierung des Motors
     #  negativ für eine Bewegung vorwärts sein muss
     def set_left_speed(self, speed):
-        self.motor_left.start(speed * -1)
-       
+        self.motor_left_1.start(speed * (-1))
+        self.motor_left_2.start(speed * (-1))
+
+
     #  Setzt die Geschwindigkeit vom linken Motor (Werte von -100 bis 100)
     def set_right_speed(self, speed):
-        self.motor_right.start(speed)
-    
-    # Setzt die Geschwindigkeit beider Motoren
+        self.motor_right_1.start(speed)
+        self.motor_right_2.start(speed)
+
+    # Setzt die Geschwindigkeit beider Motoren und merk sich die eingestellte Geschwindigkeit
     def set_speed(self, x):
+        self.speed = x
         self.set_left_speed(x)
         self.set_right_speed(x)
-    
+        
     # Dreht den Roboter je nach gegebenem Wert von -100 (links) und 100 (rechts) 
     def turn(self, x):
-        speed_left = -50 if x < 0 else x
-        speed_right = -50 if x > 0 else -x
+        speed_left = x
+        speed_right = -x
+        if x == 0:
+            speed_left, speed_right = self.speed, self.speed
         self.set_left_speed(speed_left)
         self.set_right_speed(speed_right)
+    
+    # Lenkt den Roboter indem es den entsprechenden Motor um 1 - x Protzent verlangsamt
+    def steer(self, x):
+        percentage = -30 #self.speed * (1 - abs(x/100))
+        if x < 0:
+            self.set_left_speed(percentage)
+            self.set_right_speed(self.speed)
+        else:
+            self.set_left_speed(self.speed)
+            self.set_right_speed(percentage)
         
-    # Piepst für  x Sekunden
-    # NOCH NICHT FERTIG; HÄLT DEN CODE FÜR DIE ZEIT DES PIEPSENS AN!!!!!!!!!!!!
-    def beep_for_seconds(self, seconds):
-        GPIO.output(self.BUZZER_PIN, GPIO.HIGH)
-        print("started beeping")
+    def stop_motors(self):
+        self.motor_left_1.stop()
+        self.motor_left_2.stop()
+        self.motor_right_1.stop()
+        self.motor_right_2.stop()
+
+
+    
+    # Dreht die Lenkung je nach gegebenem Wert von -100 (links) bis 100(rechts)
+    """def steer(self, x):
+        degrees = round(-0.5*x)
+        self.steering.run_to_position(degrees)
+        
+        #erster Test, Prozent Geschwindigkeitunterschied aus Gradzahl
+        percentage = x*2.0                 
+        
+        limit_low = 30 if self.speed >= 0 else -100
+        limit_high = 100 if self.speed >= 0 else -30
+        
+        # es fehlt min und max
+        wheel_left_speed = min(max(limit_low, self.speed * (1+percentage/100)), limit_high)
+        wheel_right_speed = min(max(limit_low, self.speed * (1-percentage/100)), limit_high)
+        
+        self.set_left_speed(wheel_left_speed)
+        self.set_right_speed(wheel_right_speed)"""
+
+    # Die Funktion, die die light_for_seconds Funktion als Thread startet
+    def _light_for_seconds(self, seconds):
+        print("Led on")
+        GPIO.output(self.LED_PIN, GPIO.HIGH)
         sleep(seconds)
-        GPIO.output(self.BUZZER_PIN, GPIO.LOW)
+        GPIO.output(self.LED_PIN, GPIO.LOW)
+
+    # Piepst für  x Sekunden
+    def light_for_seconds(self, seconds):
+        beep_thread = threading.Thread(target = self._light_for_seconds, args = (seconds, ))
+        beep_thread.start()
+        
+    def test_motors(self):
+        for x in range(-100, 100, 10):
+            print(f"speed = {x}")
+            self.set_speed(x)
+            sleep(5)
+            
+if __name__ == "__main__":
+    print("Remote Controller: Steuerung nach links und rechts, gebe Werte von -100 bis 100 ein")
+    robot = Robot()
+    robot.light_for_seconds(10)
+    robot.set_speed(50)
+    #robot.test_motors()
+    #steering_target = 0
+    #steering_thread = threading.Thread(target = lambda robot: (robot.steer(steering_target)), daemon=True, args=(robot,))
+    #steering_thread.start()
+    try:
+       while True:
+        try:        
+            a = int(input())
+        except ValueError:
+            print("Ungültige Nummer, gebe eine Nummer von -100 bis 100 ein")
+            continue
+        robot.steer(a)
+        #robot.steering.run_to_position(int(input()), speed = 100)
+    except KeyboardInterrupt:
+        #steering_thread.join()
+        robot.stop_motors()
         
